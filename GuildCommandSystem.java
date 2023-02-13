a64ef8e936bd2e78b8c1e0b42adfdedbce4c7e77
Carlos Seda <carlosseda123@gmail.com>
2023-02-13 13:55:54 -0500

    Insert Guild Files

--- GuildCommandSystem.java
+++ GuildCommandSystem.java
@@ -1,3 +1,17 @@
 public class GuildCommandSystem {
-    
+
+    String[] command;
+    int index=0;
+    public GuildCommandSystem(String[] args) {
+        command = args;
+    }
+
+    public boolean hasNextCommand() {
+        return index<command.length;
+    }
+
+    public GuildCommand nextCommand() {
+        index++;
+        return new GuildCommand(command[index--]);
+    }
 }
---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("*-------------------------------*");
+        System.out.println("Welcome to the Management of the guild");
+        System.out.println("*-------------------------------*");
+        // stuff to do
+        System.out.println("*-------------------------------*");
+        System.out.println("Thank you for using the system!");
+        System.out.println("*-------------------------------*");
+    }
+ }
