4a62dd7d05388dbd1d6b60c2e320c54670e97f3f
Carlos Seda <carlosseda123@gmail.com>
2023-02-13 14:39:07 -0500

    Change main

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String args[]) {
+        GuildCommandSystem arguments = new GuildCommandSystem(args);
+        GuildCommand commands = new GuildCommand(arguments.command[arguments.index]);
+        while(arguments.hasNextCommand()){
+            System.out.println("Name: " + commands.getName());
+            commands.nextString();
+            // System.out.println("args[" + i + "]: " + args[i]);
+        }
         System.out.println("*-------------------------------*");
         System.out.println("Welcome to the Management of the guild");
         System.out.println("*-------------------------------*");
